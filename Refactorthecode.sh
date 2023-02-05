6b0c8e8999944f5008f4f390fbd69ba06dd05649
mohan sairam <wifiwarriors10@gmail.com>
2023-02-06 01:23:20 +0530

    calculating wages till number of working days

---
+++ Refactorthecode.sh
@@ -0,0 +1,29 @@
+IS_PART_TIME=1;
+IS_FULL_TIME-2;
+MAX_HRS_IN_MONTH=4;
+
+EMP_RATE_PER_HR=20; 
+NUM_WORKING_DAYS=20;
+totalEmpHR=0;
+totalWorkingDays=0;
+
+function petworkingHours() {
+
+case $1 in
+
+$IS_FULL_TIME)
+workinghours=0;;
+
+$IS PART TIME)
+workinghours=4;;
+esac
+echo $workHours
+}
+while {{ $totalworkHours -It $MAX_MRS_IN_MONTH && $totalworkingDays -It $NUM_WORKING_DAYS }}
+
+do
+((totalworkingDays++))
+
+work ours=*$ ( getworking Hours $((RANDOMN)) }" totalworkours-$(($totalWorkHours = $workhours))
+
+totalSalary $(($totalWorkHours+[MP_RATE_PER_HR));
