a1718022657de44c04fd14447474cc507705d601
mohan sairam <wifiwarriors10@gmail.com>
2023-02-06 02:09:29 +0530

    storing daily wage

---
+++ storeTotalwage.sh
@@ -0,0 +1,21 @@
+IS PART TIMES:
+
+MAX HAS IN MONTH E RATE PER
+
+VARIABLES
+
+function petworking) (
+
+SIS FULL TIME
+
+SIS PART TIME
+
+g
+
+StotalMerkingDays -It NM MORKING DAYS 13
+
+pelygel "StetalwerkingDays"1" calealyage kurs 3
+
+tatalsalary caleballyage Stalworkouts
+
+echo "ALL Keys Strengbully(1)
