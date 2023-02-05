ec4c15f9687f65c3076f335f3ea72bc8f7b099b9
mohan sairam <wifiwarriors10@gmail.com>
2023-02-06 02:02:53 +0530

    storage daily wage in array

--- uc8.sh
+++ uc8.sh
@@ -22,7 +22,7 @@ IS_PART_TIME=1;
 IS_FULL_TIME-2;
 MAX_HRS_IN_MONTH=4;
 
-EMP_RATE_PER_HR=20; 
+EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 totalEmpHR=0;
 totalWorkingDays=0;
@@ -44,6 +44,7 @@ while {{ $totalworkHours -It $MAX_MRS_IN_MONTH && $totalworkingDays -It $NUM_WOR
 do
 ((totalworkingDays++))
 
-work ours=*$ ( getworking Hours $((RANDOMN)) }" totalworkours-$(($totalWorkHours = $workhours))
 
-totalSalary $(($totalWorkHours+[MP_RATE_PER_HR));
+
+
+
